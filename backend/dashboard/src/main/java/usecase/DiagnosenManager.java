169bdf72aff95a696a6ac617a60877301ca57419
kai <kai@debian-linux>
2018-10-30 13:28:57 +0100

    Entitys: Diagnose erstellt, Patient in Datenstruktur eingefügt
    UseCase DiagnosenManager
    
    Packages erstellt

---
+++ backend/dashboard/src/main/java/usecase/DiagnosenManager.java
@@ -0,0 +1,15 @@
+package usecase;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import entity.Diagnose;
+
+public class DiagnosenManager {
+	private Collection<Diagnose> diagnosen;
+	
+	public DiagnosenManager() {
+		this.diagnosen = new ArrayList<Diagnose>;
+	}
+	public boolean leseCSVein
+}
