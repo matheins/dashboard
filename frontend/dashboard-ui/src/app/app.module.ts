698d95d57fd7fa038cd3972c4e161efb0ca9606a
Matyas Heins <MatyasHeins@Matyass-MBP.fritz.box>
2018-11-05 23:55:07 +0100

    Generated first models + Dashboard View

--- frontend/dashboard-ui/src/app/app.module.ts
+++ frontend/dashboard-ui/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientComponent } from './general/patient/patient.component';
+import { PatientenComponent } from './views/patienten/patienten.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientComponent,
+    PatientenComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/dashboard-ui/src/app/views/patienten/patienten.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patienten',
+  templateUrl: './patienten.component.html',
+  styleUrls: ['./patienten.component.css']
+})
+export class PatientenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
