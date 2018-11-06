5ae48a827bfbb910a38951657b90cecfd40b98de
Matyas Heins <MatyasHeins@Matyass-MBP.fritz.box>
2018-11-06 14:43:30 +0100

    Added Chart.js and 2 Demo-Charts

--- frontend/dashboard-ui/src/app/app-routing.module.ts
+++ frontend/dashboard-ui/src/app/app-routing.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PatientenComponent } from './views/patienten/patienten.component';
+import { PieChartComponent } from './views/pie-chart/pie-chart.component';
+import { BarChartComponent } from './views/bar-chart/bar-chart.component';
 
 const routes: Routes = [
   {
     path: 'patienten',
     component: PatientenComponent,
-  }
+  },
+  {
+    path: 'pie-chart',
+    component: PieChartComponent,
+  },
+  {
+    path: 'bar-chart',
+    component: BarChartComponent,
+  },
 ];
 
 @NgModule({
--- frontend/dashboard-ui/src/app/app.module.ts
+++ frontend/dashboard-ui/src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { PatientenComponent } from './views/patienten/patienten.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PatientService } from './services/patient.service';
-
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { PieChartComponent } from './views/pie-chart/pie-chart.component';
+import { BarChartComponent } from './views/bar-chart/bar-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PatientenComponent
+    PatientenComponent,
+    PieChartComponent,
+    BarChartComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ChartsModule
   ],
   providers: [PatientService],
   bootstrap: [AppComponent]
