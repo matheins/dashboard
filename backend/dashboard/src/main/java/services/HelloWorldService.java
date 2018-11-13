91ebff78832828d9b1c27ccdb7e83fb9c975c505
MatyasHeins <MatyasHeins@83adef7a.funky.hs-osnabrueck.de>
2018-11-13 12:34:07 +0100

    Responses + RESTful Service

--- backend/dashboard/src/main/java/services/HelloWorldService.java
+++ backend/dashboard/src/main/java/services/HelloWorldService.java
@@ -2,17 +2,17 @@ package services;
 import static spark.Spark.*;
 
 public class HelloWorldService {
-	public static void main(String[] args) {
-		  
-        get("/hello", (req, res)->"Hello, world");
-         
-        get("/hello/:name", (req,res)->{
-            return "Hello, "+ req.params(":name");
-        });
-        
-        get("/hello/:name", (req,res)->{
-            return "Hello, "+ req.params(":name");
-        });
-    }
+//	public static void main(String[] args) {
+//		  
+//        get("/hello", (req, res)->"Hello, world");
+//         
+//        get("/hello/:name", (req,res)->{
+//            return "Hello, "+ req.params(":name");
+//        });
+//        
+//        get("/hello/:name", (req,res)->{
+//            return "Hello, "+ req.params(":name");
+//        });
+//    }
 
 }
---
+++ backend/dashboard/src/main/java/services/RESTful/RESTfulService.java
@@ -0,0 +1,19 @@
+package services.RESTful;
+import static spark.Spark.*;
+
+import com.google.gson.Gson;
+
+import services.PatientenService;
+import services.responses.StandardResponse;
+import services.responses.StatusResponse;
+
+public class RESTfulService {
+	public static void main(String[] args) {
+	 get("/patienten", (request, response) -> {
+		 response.type("application/json");
+		    return new Gson().toJson(
+		      new StandardResponse(StatusResponse.SUCCESS,new Gson()
+		        .toJsonTree(PatientenService.getAllPatienten())));
+     });
+	}
+}
