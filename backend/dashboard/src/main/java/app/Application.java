e0f51c7395101be0b068dbc0f1d183684573f221
johannesbauning <johannesbauning@83adecdb.funky.hs-osnabrueck.de>
2018-11-13 13:53:01 +0100

    Entitys angepasst, Notfall heit nun Aufenthalt

--- backend/dashboard/src/main/java/app/Application.java
+++ backend/dashboard/src/main/java/app/Application.java
@@ -28,7 +28,7 @@ public class Application {
 			Aufenthalt n1= new Aufenthalt("h2v3eb23vh23j",1, new Date(2019, 5, 19, 20, 15), new Date(2020, 1, 1, 12, 12),"Helikopter","49076",25);
 			Aufenthalt n2= new Aufenthalt("j2k3b42j23432",4, new Date( 2018, 7, 23, 9, 15) ,new Date (2018,  7, 23, 12, 30),"Selbst","64338",23);
 			Aufenthalt n3= new Aufenthalt("jk23b4b23b423",2, new Date( 2010, 04, 11, 15, 00),new Date( 2010, 04, 13, 14, 00) ,"Rettungswagen","34212",45);
-			//
+			
 				
 		
 			System.out.println("Aufenthalt 1:  "+	n1.getAufenthaltID() +
